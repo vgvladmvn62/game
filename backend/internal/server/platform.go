e6ccab32c35f982b20a50f7ca1e49ebe969e1e77
mjakobczyk <m.jakob@wp.pl>
2019-10-03 15:30:09 +0200

    Add mosquitto dependency, upgrade Docker Compose, add logging to server, log MQTT errors intead of failing, improve deployment yamls

--- backend/internal/server/platform.go
+++ backend/internal/server/platform.go
@@ -8,7 +8,6 @@ func (s *Server) turnOffLightsHandler(w http.ResponseWriter, r *http.Request) {
 	err := s.hardwareService.TurnOffLights()
 	if err != nil {
 		_ = NewAPIError(err.Error(), http.StatusInternalServerError).Send(w)
-		return
 	}
 
 	w.WriteHeader(http.StatusOK)
